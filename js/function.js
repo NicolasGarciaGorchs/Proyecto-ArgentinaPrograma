4208fff55acd6e8fac26878a27e7446a0314ca37
Nicolas Garcia Gorchs <nicolasgarciagorchs@gmail.com>
2023-02-28 13:37:28 -0300

    Subida a GITHUB

---
+++ js/function.js
@@ -0,0 +1,141 @@
+document.getElementById('interaccion_redes').addEventListener('click', function() {
+    document.getElementById('boton').style.display = 'flex';})
+
+    document.getElementById('guardar_redes').addEventListener('click', function() {
+        document.getElementById('boton').style.display = 'none';})
+
+
+
+
+
+        document.getElementById('interaccion_premios').addEventListener('click', function() {
+            document.getElementById('premios').style.display = 'grid';})
+
+            document.getElementById('guardar_premios').addEventListener('click', function() {
+                document.getElementById('premios').style.display = 'none';})
+
+            document.getElementById('interaccion_premios1').addEventListener('click', function() {
+                document.getElementById('premios1').style.display = 'grid';})
+                
+                document.getElementById('guardar_premios1').addEventListener('click', function() {
+                    document.getElementById('premios1').style.display = 'none';})
+
+                document.getElementById('interaccion_premios2').addEventListener('click', function() {
+                    document.getElementById('premios2').style.display = 'grid';})
+
+                    document.getElementById('guardar_premios2').addEventListener('click', function() {
+                        document.getElementById('premios2').style.display = 'none';})
+                
+                    document.getElementById('interaccion_premios3').addEventListener('click', function() {
+                        document.getElementById('premios3').style.display = 'grid';})
+
+                        document.getElementById('guardar_premios3').addEventListener('click', function() {
+                            document.getElementById('premios3').style.display = 'none';})
+
+                        document.getElementById('interaccion_premios4').addEventListener('click', function() {
+                            document.getElementById('premios4').style.display = 'grid';})
+
+                            document.getElementById('guardar_premios4').addEventListener('click', function() {
+                                document.getElementById('premios4').style.display = 'none';})
+
+                            document.getElementById('interaccion_premios5').addEventListener('click', function() {
+                                document.getElementById('premios5').style.display = 'grid';})
+
+                                document.getElementById('guardar_premios5').addEventListener('click', function() {
+                                    document.getElementById('premios5').style.display = 'none';})
+
+                                document.getElementById('interaccion_premios6').addEventListener('click', function() {
+                                    document.getElementById('premios6').style.display = 'grid';})
+
+                                    document.getElementById('guardar_premios6').addEventListener('click', function() {
+                                        document.getElementById('premios6').style.display = 'none';})
+
+                                    document.getElementById('interaccion_premios7').addEventListener('click', function() {
+                                        document.getElementById('premios7').style.display = 'grid';})
+
+                                        document.getElementById('guardar_premios7').addEventListener('click', function() {
+                                            document.getElementById('premios7').style.display = 'none';})
+
+                                        document.getElementById('interaccion_premios8').addEventListener('click', function() {
+                                            document.getElementById('premios8').style.display = 'grid';})
+
+                                            document.getElementById('guardar_premios8').addEventListener('click', function() {
+                                                document.getElementById('premios8').style.display = 'none';})
+
+                                            document.getElementById('interaccion_premios9').addEventListener('click', function() {
+                                                document.getElementById('premios9').style.display = 'grid';})
+
+                                                document.getElementById('guardar_premios9').addEventListener('click', function() {
+                                                    document.getElementById('premios9').style.display = 'none';})
+
+                                                document.getElementById('interaccion_premios10').addEventListener('click', function() {
+                                                    document.getElementById('premios10').style.display = 'grid';})
+
+                                                    document.getElementById('guardar_premios10').addEventListener('click', function() {
+                                                        document.getElementById('premios10').style.display = 'none';})
+
+                                                    document.getElementById('interaccion_premios11').addEventListener('click', function() {
+                                                        document.getElementById('premios11').style.display = 'grid';})
+
+                                                        document.getElementById('guardar_premios11').addEventListener('click', function() {
+                                                            document.getElementById('premios11').style.display = 'none';})
+
+                                                        document.getElementById('interaccion_premios12').addEventListener('click', function() {
+                                                            document.getElementById('premios12').style.display = 'grid';})
+
+                                                            document.getElementById('guardar_premios12').addEventListener('click', function() {
+                                                                document.getElementById('premios12').style.display = 'none';})
+
+                                                            document.getElementById('interaccion_premios13').addEventListener('click', function() {
+                                                                document.getElementById('premios13').style.display = 'grid';})
+
+                                                                document.getElementById('guardar_premios13').addEventListener('click', function() {
+                                                                    document.getElementById('premios13').style.display = 'none';})
+
+                                                                document.getElementById('interaccion_premios14').addEventListener('click', function() {
+                                                                    document.getElementById('premios14').style.display = 'grid';})
+
+                                                                    document.getElementById('guardar_premios14').addEventListener('click', function() {
+                                                                        document.getElementById('premios14').style.display = 'none';})
+
+                                                                    document.getElementById('interaccion_premios15').addEventListener('click', function() {
+                                                                        document.getElementById('premios15').style.display = 'grid';})
+
+                                                                        document.getElementById('guardar_premios15').addEventListener('click', function() {
+                                                                            document.getElementById('premios15').style.display = 'none';})
+
+                                                                        document.getElementById('interaccion_premios16').addEventListener('click', function() {
+                                                                            document.getElementById('premios16').style.display = 'grid';})
+
+                                                                    
+                                                                        document.getElementById('guardar_premios16').addEventListener('click', function() {
+                                                                            document.getElementById('premios16').style.display = 'none';})
+
+                                                                                    document.getElementById('interaccion_premios17').addEventListener('click', function() {
+document.getElementById('premios17').style.display = 'grid';})
+
+document.getElementById('guardar_premios17').addEventListener('click', function() {
+    document.getElementById('premios17').style.display = 'none';})
+
+document.getElementById('interaccion_premios18').addEventListener('click', function() {
+                                                                                    document.getElementById('premios18').style.display = 'grid';})
+
+                                                                                    
+                                                                                    document.getElementById('guardar_premios18').addEventListener('click', function() {
+                                                                                        document.getElementById('premios18').style.display = 'none';})
+                                                                                                                                                                        document.getElementById('interaccion_premios19').addEventListener('click', function() {
+                                                                                        document.getElementById('premios19').style.display = 'grid';})
+
+                                                                                        document.getElementById('guardar_premios19').addEventListener('click', function() {
+                                                                                            document.getElementById('premios19').style.display = 'none';})
+
+                                                                                    document.getElementById('interaccion_premios20').addEventListener('click', function() {
+                                                                                        document.getElementById('premios20').style.display = 'grid';})
+
+                                                                                        document.getElementById('guardar_premios20').addEventListener('click', function() {
+                                                                                            document.getElementById('premios20').style.display = 'none';})
+
+                                                                                    document.getElementById('interaccion_premios21').addEventListener('click', function() {
+                                                                                        document.getElementById('premios21').style.display = 'grid';})
+                                                                                        document.getElementById('guardar_premios21').addEventListener('click', function() {
+                                                                                            document.getElementById('premios21').style.display = 'none';})
